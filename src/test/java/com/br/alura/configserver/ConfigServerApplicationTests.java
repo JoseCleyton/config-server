11666d433690f1562a962f214681306b5ccbe61f
jose.de.araujo.souza <jose.de.araujo.souza@avanade.com>
2022-05-05 11:58:38 -0300

    Config Server

---
+++ src/test/java/com/br/alura/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alura.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
